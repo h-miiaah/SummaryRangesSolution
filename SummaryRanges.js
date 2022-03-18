0892980a86ac10628325d71d7771a53deed2bf82
h-miiaah <haim.miiaah5@gmail.com>
2022-03-17 22:10:55 -0400

    comments

--- SummaryRanges.js
+++ SummaryRanges.js
@@ -29,6 +29,7 @@
    * If start is null, we set start to the current value of nums[i].
    * If nums[i] is equal to nums[i + 1] - 1, we continue to the next iteration.
    * If nums[i] is equal to start, we push the value of start to the result array.
+   * If nums[i] is not equal to start, we push the string representation of the range of start to nums[i] to the result array.
    * 
    */
   
